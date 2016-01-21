3e3b7c94363ac4425d7a57a35a8ee14d727e2b75
Goshine <greenlig@gmail.com>
2016-01-21 17:35:48 +0000

    debuging

---
+++ app/mock/config.js
@@ -0,0 +1,11 @@
+var config = {};
+/**
+ * @warning do not use my zoopla api key
+ * if you would like to mock about with this project
+ * get your you own key over by zoopla for free
+ *
+ */
+config.LISTINGS_URL = '/z/api/v1/property_listings.json?api_key=pjub5ry8th4n9tvsvjkfm8tm';
+config.LISTINGS_ACOM_URL = '/z/api/v1/geo_autocomplete.json?api_key=pjub5ry8th4n9tvsvjkfm8tm';
+
+export default config;
